363acb50022b8e25b2ea1f650825f3a65420fe9b
ramizkazak <ramizkazak@gmail.com>
2020-11-10 11:57:59 -0600

    Added more message

--- src/Hello.java
+++ src/Hello.java
@@ -1,4 +1,14 @@
 public class Hello {
+    public static void main(String[] args) {
+
+
     // more stuff
     // Mac and Cheese users are happy to see commit dialog finally and finally
+
+    // I have already published on GitHub.com
+    //now i m ready tyo make more commits
+    //but do i need to share again???
+    //absolutely no
+    //just push the button
+    }
 }
